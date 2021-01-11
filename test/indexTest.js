59966e10a4e7b4643193d600f63a2516d15dc2b2
Taylor Bustamante <taylorbustamante16@gmail.com>
2021-01-11 15:21:12 -0800

    commit tree 219e3494ef3ae353ed83f3cc08906e9aa7ca33f6

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,40 +1,41 @@
-describe('Fix the Scope', function() {
-  describe('declare customerName to be bob in global scope', function() {
-    it('returns the customerName', function() {
-      expect(window.customerName).to.equal('bob');
+describe("Fix the Scope", function () {
+  describe("declare customerName to be bob in global scope", function () {
+    it("returns the customerName", function () {
+      expect(window.customerName).to.equal("bob");
     });
   });
 
-  describe('upperCaseCustomerName()', function() {
-    it('modifies the customerName variable', function() {
-      expect(window.customerName).to.equal('bob');
+  describe("upperCaseCustomerName()", function () {
+    it("modifies the customerName variable", function () {
+      expect(window.customerName).to.equal("bob");
 
       upperCaseCustomerName();
-
-      expect(window.customerName).to.equal('BOB');
+      expect(window.customerName).to.equal("BOB");
     });
   });
-  describe('setBestCustomer()', function() {
-    it('setBestCustomer', function() {
+  describe("setBestCustomer()", function () {
+    it("setBestCustomer", function () {
       expect(window.bestCustomer).to.equal(undefined);
 
       setBestCustomer();
 
-      expect(window.bestCustomer).to.equal('not bob');
+      expect(window.bestCustomer).to.equal("not bob");
     });
   });
 
-  describe('overwriteBestCustomer()', function() {
-    it('overwrites the best customer', function() {
-      overwriteBestCustomer('maybe bob');
+  describe("overwriteBestCustomer()", function () {
+    it("overwrites the best customer", function () {
+      overwriteBestCustomer("maybe bob");
 
-      expect(window.bestCustomer).to.equal('maybe bob');
+      expect(window.bestCustomer).to.equal("maybe bob");
     });
   });
 
-  describe('changeLeastFavoriteCustomer()', function() {
-    it('unsuccessfully tries to reassign the least favorite customer', function() {
-      expect(changeLeastFavoriteCustomer).to.throw('Assignment to constant variable.');
+  describe("changeLeastFavoriteCustomer()", function () {
+    it("unsuccessfully tries to reassign the least favorite customer", function () {
+      expect(changeLeastFavoriteCustomer).to.throw(
+        "Assignment to constant variable."
+      );
     });
   });
 });
